d3b8518a102fdb2c1df377ea9b0ed8b9f85770b6
Michael Kornblum <mkornblum71@gmail.com>
2019-11-05 13:41:45 -0600

    add edit routes

--- routes/field.js
+++ routes/field.js
@@ -1,26 +1,26 @@
-const express = require("express");
+const express = require('express');
 
 const {
-    getFieldIndex,
-    // getEditField,
-    getDeleteField,
-    postAddField,
-    postMoveUpField,
-    postMoveDownField,
-    // postEditField,
-    postDeleteField
-} = require("../controllers/field");
+	getFieldIndex,
+	getEditField,
+	getDeleteField,
+	postAddField,
+	postMoveUpField,
+	postMoveDownField,
+	// postEditField,
+	postDeleteField,
+} = require('../controllers/field');
 
 const router = express.Router();
 
-router.get("/fields", getFieldIndex);
-// router.get("/field/edit", getEditField);
-router.get("/field/delete", getDeleteField);
+router.get('/fields', getFieldIndex);
+router.get('/field/edit', getEditField);
+router.get('/field/delete', getDeleteField);
 
-router.post("/field/add", postAddField);
-router.post("/field/move-up", postMoveUpField);
-router.post("/field/move-down", postMoveDownField);
+router.post('/field/add', postAddField);
+router.post('/field/move-up', postMoveUpField);
+router.post('/field/move-down', postMoveDownField);
 // router.post("/collection/edit", postEditField);
-router.post("/field/delete", postDeleteField);
+router.post('/field/delete', postDeleteField);
 
 module.exports = router;
