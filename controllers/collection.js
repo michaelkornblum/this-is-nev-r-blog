8bd27003dd98834636f4128de4cbecd6e7b3c6e2
Michael Kornblum <mkornblum71@gmail.com>
2019-11-25 23:25:20 -0600

    flesh out collection ui

--- controllers/collection.js
+++ controllers/collection.js
@@ -1,55 +1,125 @@
-const Collection = require("../models/Collection");
-const Field = require("../models/Field");
+const Collection = require('../models/Collection');
+const Field = require('../models/Field');
 
 exports.getCollectionIndex = (req, res) =>
-    Collection.fetchAll(collections =>
-        res.render("collection/index", {
-            pageTitle: "collections",
-            collections,
-            collectionId: null,
-            isEditing: false,
-            isDeleting: false
-        })
-    );
+	Collection.fetchAll(collections =>
+		res.render('collection/index', {
+			pageTitle: 'Collections',
+			collections,
+			collectionId: null,
+			collectionName: null,
+			isEditing: false,
+			isDeleting: false,
+			wasAdded: false,
+			wasEdited: false,
+			wasDeleted: false,
+		}),
+	);
 
 exports.getEditCollection = (req, res) =>
-    Collection.fetchAll(collections =>
-        res.render("collection/index", {
-            pageTitle: "collections",
-            collections,
-            collectionId: req.query.collectionId,
-            isEditing: true,
-            isDeleting: false
-        })
-    );
+	Collection.fetchAll(collections =>
+		res.render('collection/index', {
+			pageTitle: 'Collections',
+			collections,
+			collectionId: req.query.collectionId,
+			collectionName: null,
+			isEditing: true,
+			isDeleting: false,
+			wasAdded: false,
+			wasEdited: false,
+			wasDeleted: false,
+		}),
+	);
 
 exports.getDeleteCollection = (req, res) =>
-    Collection.fetchAll(collections =>
-        res.render("collection/index", {
-            pageTitle: "collections",
-            collections,
-            collectionId: req.query.collectionId,
-            isEditing: false,
-            isDeleting: true
-        })
-    );
+	Collection.fetchAll(collections =>
+		Collection.findById(req.query.collectionId, collection =>
+			res.render('collection/index', {
+				pageTitle: 'Collections',
+				collections,
+				collectionId: req.query.collectionId,
+				collectionName: collection.name,
+				isEditing: false,
+				isDeleting: true,
+				wasAdded: false,
+				wasEdited: false,
+				wasDeleted: false,
+			}),	
+		)
+	);
+
+exports.getAddedCollection = (req, res) =>
+	Collection.fetchAll(collections =>
+		res.render('collection/index', {
+			pageTitle: 'Collections',
+			collections,
+			collectionId: null,
+			collectionName: req.query.collectionName,
+			isEditing: false,
+			isDeleting: false,
+			wasAdded: true,
+			wasEdited: false,
+			wasDeleted: false,
+		}),
+	);
+
+exports.getEditedCollection = (req, res) =>
+	Collection.fetchAll(collections =>
+		res.render('collection/index', {
+			pageTitle: 'Collections',
+			collections,
+			collectionId: null,
+			collectionName: req.query.collectionName,
+			isEditing: false,
+			isDeleting: false,
+			wasAdded: false,
+			wasEdited: true,
+			wasDeleted: false,
+		}),
+	);
+
+exports.getDeletedCollection = (req, res) =>
+	Collection.fetchAll(collections =>
+		res.render('collection/index', {
+			pageTitle: 'Collections',
+			collections,
+			collectionId: null,
+			collectionName: req.query.collectionName,
+			isEditing: false,
+			isDeleting: false,
+			wasAdded: false,
+			wasEdited: false,
+			wasDeleted: true,
+		}),
+	);
 
 exports.postAddCollection = (req, res) =>
-    new Collection(null, req.body.collectionName).save(err =>
-        err ? console.error(err) : res.redirect("/")
-    );
+	new Collection(null, req.body.collectionName).save(err =>
+		err
+			? console.error(err)
+			: res.redirect(
+					`/collection/added?collectionName=${req.body.collectionName}`,
+			),
+	);
 
 exports.postEditCollection = (req, res) =>
-    new Collection(req.body.collectionId, req.body.collectionName).save(err =>
-        err ? console.error(err) : res.redirect("/")
-    );
+	new Collection(req.body.collectionId, req.body.collectionName).save(err =>
+		err
+			? console.error(err)
+			: res.redirect(
+					`/collection/edited?collectionName=${req.body.collectionName}`,
+			),
+	);
 
 exports.postDeleteCollection = (req, res) =>
-    Field.deleteByCollectionId(req.body.collectionId, err =>
-        err
-            ? console.error(err)
-            : Collection.delete(req.body.collectionId, err =>
-                err ? console.error(err) : res.redirect("/")
-            )
-    );
-
+	Field.deleteByCollectionId(req.body.collectionId, err =>
+		err
+			? console.error(err)
+			: Collection.delete(req.body.collectionId, err =>
+					err
+						? console.error(err)
+						: res.redirect(
+								`/collection/deleted?collectionName=${req.body.collectionName}`,
+						),
+			),
+	);
--- routes/collection.js
+++ routes/collection.js
@@ -1,22 +1,29 @@
-const express = require("express");
+const express = require('express');
 
 const {
-    getCollectionIndex,
-    getEditCollection,
-    getDeleteCollection,
-    postAddCollection,
-    postEditCollection,
-    postDeleteCollection
-} = require("../controllers/collection");
+	getCollectionIndex,
+	getEditCollection,
+	getDeleteCollection,
+	getAddedCollection,
+	getEditedCollection,
+	getDeletedCollection,
+
+	postAddCollection,
+	postEditCollection,
+	postDeleteCollection,
+} = require('../controllers/collection');
 
 const router = express.Router();
 
-router.get("/", getCollectionIndex);
-router.get("/collection/edit", getEditCollection);
-router.get("/collection/delete", getDeleteCollection);
+router.get('/', getCollectionIndex);
+router.get('/collection/edit', getEditCollection);
+router.get('/collection/delete', getDeleteCollection);
+router.get('/collection/added', getAddedCollection);
+router.get('/collection/edited', getEditedCollection);
+router.get('/collection/deleted', getDeletedCollection);
 
-router.post("/collection/add", postAddCollection);
-router.post("/collection/edit", postEditCollection);
-router.post("/collection/delete", postDeleteCollection);
+router.post('/collection/add', postAddCollection);
+router.post('/collection/edit', postEditCollection);
+router.post('/collection/delete', postDeleteCollection);
 
 module.exports = router;
