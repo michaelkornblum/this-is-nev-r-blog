b35f5476801b74de1b9262467a266e77063501a3
Michael Kornblum <mkornblum71@gmail.com>
2019-11-25 23:27:01 -0600

    add entry controller

--- controllers/entry.js
+++ controllers/entry.js
@@ -1,16 +1,28 @@
 const Collection = require('../models/Collection');
-// const Field = require('../models/Field');
+const Field = require('../models/Field');
 const Entry = require('../models/Entry');
 
 exports.getEntryIndex = (req, res) =>
 	Collection.findById(req.query.collectionId, collection =>
 		Entry.findByCollectionId(collection.id, entries =>
 			res.render('entry/index', {
-				pageTitle: 'entries',
+				pageTitle: 'Entries',
 				collection,
 				entries,
 				entryId: null,
 				isDeleting: false,
+			}),
+		),
+	);
+
+exports.getAddEntry = (req, res) =>
+	Collection.findById(req.query.collectionId, collection =>
+		Field.findByCollectionId(collection.id, fields =>
+			res.render('entry/edit', {
+				pageTitle: 'New Entry',
+				collection,
+				fields: fields.sort((a, b) => a.order - b.order),
+				isEditing: false,
 			}),
 		),
 	);
